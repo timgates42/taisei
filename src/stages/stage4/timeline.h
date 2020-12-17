14ee710fb252d21f6351629865feee03a2a34f9d
Lukas Weber <laochailan@web.de>
2020-12-17 17:39:32 +0200

    stage4: new PBR background, modernize structure

---
+++ src/stages/stage4/timeline.h
@@ -0,0 +1,21 @@
+/*
+ * This software is licensed under the terms of the MIT License.
+ * See COPYING for further information.
+ * ---
+ * Copyright (c) 2011-2019, Lukas Weber <laochailan@web.de>.
+ * Copyright (c) 2012-2019, Andrei Alexeyev <akari@taisei-project.org>.
+ */
+
+#ifndef IGUARD_stages_stage4_timeline_h
+#define IGUARD_stages_stage4_timeline_h
+
+#include "taisei.h"
+
+#include "boss.h"
+
+void stage4_events(void);
+
+#define STAGE4_MIDBOSS_TIME 3724
+#define STAGE4_MIDBOSS_MUSIC_TIME 2818
+
+#endif // IGUARD_stages_stage4_timeline_h
